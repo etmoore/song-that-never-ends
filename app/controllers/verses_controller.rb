dd14f089d2f1b7f378e8f44c01afdc056dfe2d75
Evan Moore <etmoore@gmail.com>
2014-10-13 12:06:06 -0600

    started single view

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,19 +1,22 @@
 class VersesController < ApplicationController
 
-  def verse1
-    render :verse1
+  def initialize
+    @verses = ["This is the song that never ends...",
+              "Yes it goes on and on my friends...",
+              "Some people started singing it, not knowing what it was...",
+              "And they'll just keep on singing it forever just because"]
+    @verse_index = 0
+    @current_verse = @verses[@verse_index]
   end
 
-  def verse2
-    render :verse2
+  def show
+    render: verse
   end
 
-  def verse3
-    render :verse3
-  end
+  private
 
-  def verse4
-    render :verse4
+  def set_next_verse
+    @next_verse
   end
 
 end
