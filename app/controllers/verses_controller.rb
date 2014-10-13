6a07d4240ffa826cb0824c695fffc6639adaee8d
Evan Moore <etmoore@gmail.com>
2014-10-13 13:48:14 -0600

    removed constants and used session variable

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,31 +1,30 @@
 class VersesController < ApplicationController
-
-  # FIXTHIS - should not be using globals
-  $verses = ["This is the song that never ends...",
+# FIXTHIS - should not be using globals
+  VERSES = ["This is the song that never ends...",
             "Yes it goes on and on my friends...",
             "Some people started singing it, not knowing what it was...",
-            "And they'll just keep on singing it forever just because"]
-  $verse_index = 0
-  $next_verse = $verses[$verse_index]
+            "And they'll just keep on singing it forever just because..."]
 
   def show
-    render :verse
+    session[:next_verse] ||= -1
     increment_index
     get_next_verse
+    render :verse
   end
 
   private
 
   def increment_index
-    if $verse_index < 3
-      $verse_index += 1
+
+    if session[:next_verse] < 3
+      session[:next_verse] += 1
     else
-      $verse_index = 0
+      session[:next_verse] = 0
     end
   end
 
   def get_next_verse
-    $next_verse = $verses[$verse_index]
+    @next_verse = VERSES[session[:next_verse]]
   end
 
 end
